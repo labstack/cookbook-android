d52cfd185d0a13252765b8a7b18866253870ea93
Vishal Rana <vr@labstack.com>
2017-08-13 09:47:34 -0700

    Updated to latest SDK
    Signed-off-by: Vishal Rana <vr@labstack.com>

--- crash-reporter/src/main/java/com/labstack/cookbook/android/crashreporter/MainActivity.java
+++ crash-reporter/src/main/java/com/labstack/cookbook/android/crashreporter/MainActivity.java
@@ -11,20 +11,9 @@ import com.labstack.android.Client;
 import com.labstack.android.Log;
 import com.labstack.cookbook.android.R;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
 public class MainActivity extends AppCompatActivity {
     private Client client;
     private Log log;
-    private Thread.UncaughtExceptionHandler defaultUncaughtExceptionHandle;
-
-    private static String getStackTrace(Throwable throwable) {
-        StringWriter stringWriter = new StringWriter();
-        PrintWriter printWriter = new PrintWriter(stringWriter);
-        throwable.printStackTrace(printWriter);
-        return stringWriter.toString();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,21 +28,9 @@ public class MainActivity extends AppCompatActivity {
                 .add("app_name", "crash-reporter")
                 .add("app_id", InstanceID.getInstance(this).getId());
 
-        // Automatically report uncaught fatal error
-        defaultUncaughtExceptionHandle = Thread.getDefaultUncaughtExceptionHandler();
-        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-            @Override
-            public void uncaughtException(Thread thread, Throwable throwable) {
-                log.fatal(new Fields()
-                        .add("message", throwable.getMessage())
-                        .add("stack_trace", getStackTrace(throwable)));
-                defaultUncaughtExceptionHandle.uncaughtException(thread, throwable);
-            }
-        });
-
-        // Simulate crash
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        // Automatically report crash (fatal error)
+        Button crashButton = (Button) findViewById(R.id.crash_button);
+        crashButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 throw new RuntimeException("fatal error");
@@ -61,10 +38,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Manually report non-fatal error
-        try {
-            throw new Exception("non-fatal error");
-        } catch (Exception e) {
-            log.error(new Fields().add("message", e.getMessage()));
-        }
+        Button errorButton = (Button) findViewById(R.id.error_button);
+        errorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    throw new Exception("non-fatal error");
+                } catch (Exception e) {
+                    log.error(new Fields().add("message", e.getMessage()));
+                }
+            }
+        });
     }
 }
